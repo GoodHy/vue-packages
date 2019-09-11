355389c9a10f27f701f1dd0c56363cb2bea78e25
弘毅 <goodmenhy@163.com>
2019-09-11 11:45:39 +0800

    测试包

--- examples/main.js
+++ examples/main.js
@@ -2,7 +2,12 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+// main.js
 
+// 导入组件库
+import demo from '../packages/index';
+// 注册组件库
+Vue.use(demo);
 Vue.config.productionTip = false;
 
 new Vue({
