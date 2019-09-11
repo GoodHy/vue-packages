d5e4a461f4d29e14c8dcea1d9cdfa5c2ff6d2076
弘毅 <goodmenhy@163.com>
2019-09-11 15:03:52 +0800

    npm测试包

--- packages/index.js
+++ packages/index.js
@@ -2,39 +2,22 @@
 import demoComponent from "./demo-component/src/demo-component.vue";
 
 // 存储组件列表
-// const components = [demoComponent];
-// // 定义 install 方法，接收 Vue 作为参数。如果使用 use 注册插件，则所有的组件都将被注册
-// const install = function (Vue) {
-//   // 判断是否安装
-//   if (install.installed) return;
-//   // 遍历注册全局组件
-//   components.forEach(component => {
-//     Vue.component(component.name, component)
-//   });
-// };
-// // 判断是否是直接引入文件
-// if (typeof window !== "undefined" && window.Vue) {
-//   install(window.Vue);
-// }
-// export default {
-//   // 导出的对象必须具有 install，才能被 Vue.use() 方法安装
-//   install,
-//   // 以下是具体的组件列表
-//   demoComponent
-// };
-// Declare install function executed by Vue.use()
-export function install(Vue) {
+const components = [demoComponent];
+// 定义 install 方法，接收 Vue 作为参数。如果使用 use 注册插件，则所有的组件都将被注册
+const install = function (Vue) {
+  // 判断是否安装
   if (install.installed) return;
-  install.installed = true;
-  Vue.component('MyComponent', demoComponent);
-}
-
+  // 遍历注册全局组件
+  components.forEach(component => {
+    Vue.component(component.name, component)
+  });
+};
 // Create module definition for Vue.use()
 const plugin = {
   install,
 };
 
-// Auto-install when vue is found (eg. in browser via <script> tag)
+// 当发现vue的时候自动下载
 let GlobalVue = null;
 if (typeof window !== 'undefined') {
   GlobalVue = window.Vue;
@@ -44,6 +27,9 @@ if (typeof window !== 'undefined') {
 if (GlobalVue) {
   GlobalVue.use(plugin);
 }
-
-// To allow use as module (npm/webpack/etc.) export component
-export default demoComponent;
+export default {
+  // 导出的对象必须具有 install，才能被 Vue.use() 方法安装
+  install,
+  // 以下是具体的组件列表
+  demoComponent
+};
